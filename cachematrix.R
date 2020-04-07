f8fa6e1f93ce74bd16ad758d2495ee149e298798
danielwu04 <57278628+danielwu04@users.noreply.github.com>
2020-04-06 18:07:45 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,27 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+  inverse <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    inverse <<- NULL
+  }
+  inverse <<- solve(x)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+    if(!is.null(inverse))
+    {
+      return(inverse)
+    }
+  solve(x)
+          ## Return a matrix that is the inverse of 'x'
+  
 }
